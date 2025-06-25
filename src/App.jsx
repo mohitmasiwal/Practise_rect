9434994b05aebadc27f5944a3f4ac9208b1cfc06
mohitmasiwal <mohitmasiwal02@gmail.com>
2025-06-25 16:22:01 +0530

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
+
+function outer(){
+ let  count=0;
+  return function inner(){
+   console.log(count+1);
+   
+  }
+}
+let newD = outer()
+newD()
+newD()
+
 const App = () => {
   return (
     <div>
       
+  
+
+
     </div>
   )
 }
