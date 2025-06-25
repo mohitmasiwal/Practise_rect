b1fcae96fa5858700e3c6fe2e604c1dea2f3765b
mohitmasiwal <mohitmasiwal02@gmail.com>
2025-06-25 16:22:42 +0530

    we

--- src/Store.jsx
+++ src/Store.jsx
@@ -1,12 +1,1 @@
- import React from 'react'
- 
- const Store = () => {
-   return (
-     <div>
-       <h1>mohihiuggihivb</h1>
-     </div>
-   )
- }
- 
- export default Store
  
--- src/Test.jsx
+++ src/Test.jsx
@@ -1,12 +1,1 @@
- import React from 'react'
- 
- const Test = () => {
-   return (
-     <div>
-       <h1>hbkbeWEJHB</h1>
-     </div>
-   )
- }
- 
- export default Test
  
