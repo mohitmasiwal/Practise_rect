9233907ef76a83e09a575d709a584f4f9cb6d3ab
mohitmasiwal <mohitmasiwal02@gmail.com>
2025-06-27 22:03:27 +0530

    n

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,12 @@
- import React, { useReducer } from 'react'
+ import React from 'react'
  
-
- let initialstate={
-  count:0,
-  name:"counter"
-}
-
-function reducer(state ,action){
-    if(action.type === "inc"){
-      return  { ...state,  count: state.count++}
-    }
-    if(action.type === "dec"){
-      return  { count: state.count--}
-    }
-}
-
  const App = () => {
-  const [state ,dispatch] = useReducer(reducer ,initialstate) 
    return (
      <div>
-      <h1>{state.count}</h1>
-      <p>{state.name}</p>
-       <button onClick={()=> dispatch({type:"inc"})}>inc</button>
-       <button onClick={()=> dispatch({type:"dec"})}>dec</button>
-      
+       
      </div>
    )
  }
  
  export default App
-
-
- 
  
