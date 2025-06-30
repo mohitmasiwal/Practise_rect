e0d9c619ed1a04b2866d89fbec93bc130cc0a161
mohitmasiwal <mohitmasiwal02@gmail.com>
2025-06-30 09:50:22 +0530

    sd

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
- import React from 'react'
+ import React, { useState } from 'react'
  
- const 4 = () => {
+ const App = () => {
+  const [count ,setcount]= useState(0);
+  function inc(){
+    setcount(pre=>pre+5)
+  }
    return (
      <div>
-       
+       <h1>{count}</h1>
+       <h1></h1>
+       <button onClick={()=>inc()}>inc</button>
      </div>
    )
  }
  
- export default 4
+ export default App
  
