bfa01ba012fc56ac188863d8c8afa376d53fd194
mohitmasiwal <mohitmasiwal02@gmail.com>
2025-07-05 11:43:14 +0530

    xx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
  import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Fetchdata, login } from './AuthSlice'
-   
+ 
  const App = () => {
-  const dispatch = useDispatch()
-  const count = useSelector((state)=> state.auth.count)
    return (
      <div>
-      {count}
-       <button onClick={()=>dispatch(login())}>lopgin</button>
-       <button onClick={()=>dispatch(Fetchdata())}>fetch </button>
+       
      </div>
    )
  }
--- src/Store.jsx
+++ src/Store.jsx
@@ -1,7 +1,1 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./AuthSlice"
- export const store = configureStore({
-    reducer:{
-      auth:authReducer  
-    }
- })
+ 
